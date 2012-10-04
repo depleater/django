2ce97a83218e52d1a9cb384e0cc7396acb6a6e67
Peter Wright <peterw@transcitygroup.com.au>
2012-10-04 16:50:16 +1000

    Fixed dicey show-save-button(s) logic in submit_row.
    I suspect this function relied on the assumption that if an admin
    add/change form is being displayed, the user *must* at least have the
    "change" permission.

--- django/contrib/admin/templatetags/admin_modify.py
+++ django/contrib/admin/templatetags/admin_modify.py
@@ -28,17 +28,21 @@ def submit_row(context):
     change = context['change']
     is_popup = context['is_popup']
     save_as = context['save_as']
+
+    able_to_save = ((context['add'] and context['has_add_permission']) or
+            (change and context['has_change_permission']))
+
     return {
         'onclick_attrib': (opts.get_ordered_objects() and change
                             and 'onclick="submitOrderForm();"' or ''),
         'show_delete_link': (not is_popup and context['has_delete_permission']
                               and (change or context['show_delete'])),
-        'show_save_as_new': not is_popup and change and save_as,
-        'show_save_and_add_another': context['has_add_permission'] and
+        'show_save_as_new': not is_popup and change and save_as and able_to_save,
+        'show_save_and_add_another': able_to_save and
                             not is_popup and (not save_as or context['add']),
         'show_save_and_continue': not is_popup and context['has_change_permission'],
         'is_popup': is_popup,
-        'show_save': context['has_change_permission'],
+        'show_save': able_to_save,
     }
 
 @register.filter
